cf19a1aa0f871ca051ec0b63d3e1bc86e1f4c1ea
Eduardo Trujillo <eduard4444@gmail.com>
2013-11-28 18:38:23 -0500

    Finish auth code

---
+++ CarRentalService/src/main/java/edu/uga/csci4050/group3/db/SessionException.java
@@ -0,0 +1,10 @@
+package edu.uga.csci4050.group3.db;
+
+public class SessionException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
--- CarRentalService/src/main/java/edu/uga/csci4050/group3/db/SessionManagement.java
+++ CarRentalService/src/main/java/edu/uga/csci4050/group3/db/SessionManagement.java
@@ -7,6 +7,7 @@ import java.util.List;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import edu.uga.csci4050.group3.core.AuthenticationException;
 import edu.uga.csci4050.group3.core.UserEntity;
@@ -15,6 +16,7 @@ import edu.uga.csci4050.group3.core.UserType;
 public class SessionManagement {
 
 	public static final String COOKIE_NAME = "rentalAuth";
+	public static final String SESSION_NAME = "rentalAuth";
 	
 	private HttpServletRequest request;
 	private HttpServletResponse response;
@@ -25,27 +27,11 @@ public class SessionManagement {
 	}
 	
 	public String getLoggedinUsername() throws AuthenticationException{
-		// Check for the auth cookie
-		Cookie[] cookies = request.getCookies();
-		
-		// Check if we got any cookies
-		if(cookies != null){
-			String username = null;
-			
-			// Check if the auth cookie is within them
-			for(Cookie cookie : cookies){
-				if(cookie.getName().equals(COOKIE_NAME)){
-					username = cookie.getValue();
-				}
-			}
-			
-			if(username != null){
-				return username;
-			}else{
-				throw new AuthenticationException();
-			}
-		}else{
+		// Check if the session contains a user
+		if(request.getSession().getAttribute(SESSION_NAME) == null){
 			throw new AuthenticationException();
+		}else{
+			return (String)request.getSession().getAttribute(SESSION_NAME);
 		}
 	}
 	
@@ -139,5 +125,44 @@ public class SessionManagement {
 		List<UserType> roles = new ArrayList<UserType>();
 		roles.add(role);
 		return requireRole(roles, redirectUrl);
+	}
+	
+	/**
+	 * Log in a user into the system. This should be done after authentication
+	 * @param username User to login
+	 * @throws SessionException If a user is already logged in
+	 */
+	public void createSession(String username) throws SessionException{
+		// Check that there isn't a user already logged in
+		if(isUserLoggedIn()){
+			throw new SessionException();
+		}
+		
+		// Create HttpSession
+		HttpSession session = request.getSession(true);
+		session.setAttribute(SESSION_NAME, username);
+		
+		// Create session cookie
+		Cookie cookie = new Cookie(COOKIE_NAME, username);
+		
+		// Set expiration time to 60 min
+		cookie.setMaxAge(60*60);
+		session.setMaxInactiveInterval(60*60);
+		
+		// Add cookie to response
+		response.addCookie(cookie);
+	}
+	
+	/**
+	 * Logs the user off the system
+	 */
+	public void invalidateSession(){
+		// Check if there is a user logged in
+		if(!isUserLoggedIn()){
+			// Nothing to do.
+			return;
+		}
+		
+		request.getSession().invalidate();
 	}
 }
