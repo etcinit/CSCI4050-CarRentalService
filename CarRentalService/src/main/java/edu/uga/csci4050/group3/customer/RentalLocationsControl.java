bd611751a26269763b3277a08c2d99fc026f13f7
Ross Freeman <refreeman13@gmail.com>
2013-11-30 16:28:00 -0500

    Added customer package with RentalLocationControl class(unfinished)

---
+++ CarRentalService/src/main/java/edu/uga/csci4050/group3/customer/RentalLocationsControl.java
@@ -0,0 +1,17 @@
+package edu.uga.csci4050.group3.customer;
+
+import edu.uga.csci4050.group3.core.*;
+
+public class RentalLocationsControl {
+
+	// holds instance of Rental Location object returned by findRentalLocation().
+	 private LocationEntity currRentalLocation;
+	 
+	 
+	 public LocationEntity findRentalLocation(String address){
+		 
+		 
+		 return ;
+	 }
+	 
+}
