608e6a78cf1122c81fb5c13589fa0bf2bdd04195
Eduardo Trujillo <ed@chromabits.com>
2013-12-02 10:51:54 -0500

    Move location filter logic to control classes
    - The Location filter code was on a boundary class. Boundary classes
    should only contain display code.

---
+++ CarRentalService/src/main/java/edu/uga/csci4050/group3/customer/LocationFilterControl.java
@@ -0,0 +1,49 @@
+package edu.uga.csci4050.group3.customer;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+
+import edu.uga.csci4050.group3.core.*;
+import edu.uga.csci4050.group3.db.DatabaseAbstraction;
+import edu.uga.csci4050.group3.db.RecordNotFoundException;
+
+public class LocationFilterControl {
+	 
+	 public List<LocationEntity> getFilteredList(HttpServletRequest request){
+		 
+		 try{
+			 List<LocationEntity> list = DatabaseAbstraction.getLocations();
+			 List<LocationEntity> filteredList = new ArrayList<LocationEntity>();
+			 
+			 for(LocationEntity loc : list){
+				 boolean matches = true;
+				 
+				 // Check if state matches (if provided)
+				 if(request.getParameterMap().containsKey("state") && request.getParameter("state") != ""){
+					 if(!loc.getState().equals(request.getParameter("state"))){
+						 matches = false;
+					 }
+				 }
+				 
+				 // Check if zipcode matches (if provided)
+				 if(request.getParameterMap().containsKey("zipcode") && request.getParameter("zipcode") != ""){
+					 if(loc.getZipcode() != new Integer(request.getParameter("zipcode"))){
+						 matches = false;
+					 }
+				 }
+				 
+				 // If all conditions pass, the add to the filtered list
+				 if(matches == true){
+					 filteredList.add(loc);
+				 }
+			 }
+			 
+			 return filteredList;
+		 } catch( RecordNotFoundException e ){
+			 return new ArrayList<LocationEntity>();
+		 }
+	 }
+	 
+}
--- CarRentalService/src/main/java/edu/uga/csci4050/group3/customer/LocationFilterUI.java
+++ CarRentalService/src/main/java/edu/uga/csci4050/group3/customer/LocationFilterUI.java
@@ -1,55 +1,45 @@
 package edu.uga.csci4050.group3.customer;
 
 import java.util.List;
+
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import edu.uga.csci4050.group3.core.*;
+import edu.uga.csci4050.group3.template.Alert;
+import edu.uga.csci4050.group3.template.AlertType;
 import edu.uga.csci4050.group3.template.LayoutRoot;
 import edu.uga.csci4050.group3.template.SimpleTemplate;
 
-public class RentalLocationsUI implements Boundary {
+public class LocationFilterUI implements Boundary {
 	
 	public void handleRequest(HttpServletRequest request,
 			HttpServletResponse response, ServletContext context,
 			RequestType type) {
 		
 		LayoutRoot lr = new LayoutRoot(context, request, response);
-		SimpleTemplate list = new SimpleTemplate(context, "RentalLocationsList.mustache");
-		LocationListControl control = new LocationListControl();
+		SimpleTemplate list = new SimpleTemplate(context, "LocationFilterList.mustache");
+		LocationFilterControl control = new LocationFilterControl();
 		lr.setTitle("Locations");
 		
-		List<LocationEntity> locations = control.getList();
-		String userLocation = request.getParameter("state");
+		List<LocationEntity> locations = control.getFilteredList(request);
 		
 		if(locations.size() > 0){
 			String locationsHtml = "";
 			for(LocationEntity loc : locations){
 				SimpleTemplate locRow = new SimpleTemplate(context, "LocationRow.mustache");
-				if(loc.getState().compareTo(userLocation) == 0){
 				locRow.setVariables(loc.getData());
 				locationsHtml += locRow.render();
-				}
 			}
 			list.setVariable("locations", locationsHtml);
 		}else{
-			list.setVariable("message", "<h4>No locations found</h4>");
+			Alert info = new Alert(context,"No locations found with the specified criteria");
+			info.setType(AlertType.WARNING);
+			list.setVariable("message", info.render());
 		}
 		lr.setContent(list.render());
 		lr.render(response);
-		
-	}
-
-	
-	
-	
-	//uses getters and setters from LocationEntity to obtain location info and display to user,
-	//use findRentalLocation() operation.
-	public void getLocationData(LocationEntity location ){
-		
-		
-		
 		
 	}
 	
