d6d224617da09ee4a1798cd5260f6e35f5df2c40
Justin Hromalik <Hromalik@uga.edu>
2013-12-03 12:48:11 -0500

    Fixed Account Management

--- CarRentalService/src/main/java/edu/uga/csci4050/group3/core/UserAccountControl.java
+++ CarRentalService/src/main/java/edu/uga/csci4050/group3/core/UserAccountControl.java
@@ -14,40 +14,44 @@ public class UserAccountControl {
 		
 		// Get the current user
 		SessionManagement sessMan = new SessionManagement(request, response);
-		UserEntity user = null;
+		InvalidInputException iie = new InvalidInputException();
+		UserEntity currentInfo = null, newInfo = null;
+		newInfo = new UserEntity();
+		
+		
 		try {
-			user = DatabaseAbstraction.getUserByUsername(sessMan.getLoggedinUsername());
+			currentInfo = DatabaseAbstraction.getUserByUsername(sessMan.getLoggedinUsername());
 		} catch (AuthenticationException e1) {
-			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
 		
 		// Get user data from form
-		InvalidInputException iie = new InvalidInputException();
-		user.loadFromForm(request.getParameterMap());
+		newInfo.loadFromForm(request.getParameterMap());
+				
 		
-		// Check username is not taken
-		try {
-			UserEntity check = DatabaseAbstraction.getUserByUsername(user.getUsername());
-			iie.addMessage("Username is taken");
-		} catch (RecordNotFoundException e) {
-			// This is good. It's what is supposed to happen
+		// Check username is not taken if it's different
+		if (!newInfo.getUsername().equals(currentInfo.getUsername())) {
+			try {
+				UserEntity check = DatabaseAbstraction.getUserByUsername(newInfo.getUsername());
+				iie.addMessage("Username is taken");
+			} catch (RecordNotFoundException e) {
+				// This is good. It's what is supposed to happen
+			}
 		}
-		
+				
 		// Check user is over 21 years old
-		int diff = DatabaseAbstraction.getTimestampFromDate(new Date()) - user.getDateofbirth();
+		int diff = DatabaseAbstraction.getTimestampFromDate(new Date()) - newInfo.getDateofbirth();
 		if(diff < 16675200){
 			iie.addMessage("In order to register you need to be at least 21 years old");
 		}
-		
+				
 		if(iie.countMessages() > 0){
 			throw iie;
 		}
 		
-		// Validate
-		user.validate();
+		currentInfo.loadFromForm(request.getParameterMap());
 		
 		// Send to database
-		DatabaseAbstraction.putUser(user);
+		DatabaseAbstraction.updateUser(currentInfo);
 	}
 }
